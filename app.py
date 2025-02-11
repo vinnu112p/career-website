4b3ade498b350f24cd5e7a498c472165243747de
Vinayak Patel <147581522+vinnu112p@users.noreply.github.com>
2025-02-11 18:52:47 +0000

    added the flask project

--- app.py
+++ app.py
@@ -2,11 +2,11 @@ from flask import Flask
 
 app = Flask(__name__)
 
-
+print(__name__)
 @app.route('/')
 def hello_world():
-  return 'Hello, World!'
+  return 'Hello, gamer!'
 
 
 if __name__ == '__main__':
-  app.run(debug=True)
+  app.run(host='0.0.0.0', debug=True)
