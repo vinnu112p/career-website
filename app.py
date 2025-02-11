4eadb66a5086b1a546ba0b81a223ef2747eb0ee1
patelvinnu112 <38314083-patelvinnu112@users.noreply.replit.com>
2025-02-11 18:45:10 +0000

    User checkpoint: Implement basic Flask web application.

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
+from flask import Flask
 
-def main():
-    print("hello")
+app = Flask(__name__)
 
-if __name__ == "__main__":
-    main()
+
+@app.route('/')
+def hello_world():
+  return 'Hello, World!'
+
+
+if __name__ == 'main':
+  app.run(debug=True)
